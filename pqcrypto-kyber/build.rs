aba13f1f6b3ee0d46a1bc2ffee8a2c8ebb0450cf
Thom Wiggers <thom@thomwiggers.nl>
2019-04-03 14:04:16 +0200

    initial commit

---
+++ pqcrypto-kyber/build.rs
@@ -0,0 +1,23 @@
+extern crate cc;
+extern crate glob;
+
+use std::path::Path;
+
+
+fn main() {
+    let target_dir = Path::new("../pqclean/crypto_kem/kyber768/clean");
+    let common_dir = Path::new("../pqclean/common");
+    let common_files = [
+        common_dir.join("fips202.c"),
+        common_dir.join("notrandombytes.c"),
+        common_dir.join("sha2.c"),
+    ];
+
+    let scheme_files = glob::glob(target_dir.join("*.c").to_str().unwrap()).unwrap();
+    cc::Build::new()
+        .include("../pqclean/common")
+        .include(target_dir)
+        .files(common_files.into_iter())
+        .files(scheme_files.into_iter().map(|p| p.unwrap().to_string_lossy().into_owned()))
+        .compile("libkyber768.a");
+}
---
+++ pqcrypto-traits/src/kem.rs
@@ -0,0 +1,19 @@
+pub trait PublicKey {
+    fn as_bytes(&self) -> &[u8];
+    fn from_bytes(bytes: &[u8]) -> Self where Self: Sized;
+}
+
+pub trait SecretKey {
+    fn as_bytes(&self) -> &[u8];
+    fn from_bytes(bytes: &[u8]) -> Self where Self: Sized;
+}
+
+pub trait Ciphertext {
+    fn as_bytes(&self) -> &[u8];
+    fn from_bytes(bytes: &[u8]) -> Self where Self: Sized;
+}
+
+pub trait SharedSecret {
+    fn as_bytes(&self) -> &[u8];
+    fn from_bytes(bytes: &[u8]) -> Self where Self: Sized;
+}
