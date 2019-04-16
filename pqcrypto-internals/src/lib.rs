f529a4c7c37a65d4116d73a33061c339e802f1d4
Thom Wiggers <thom@thomwiggers.nl>
2019-04-16 11:43:49 +0200

    Use more efficient way to fill buffer

--- pqcrypto-internals/src/lib.rs
+++ pqcrypto-internals/src/lib.rs
@@ -3,14 +3,10 @@ use libc;
 
 #[no_mangle]
 pub extern "C" fn randombytes(buf: *mut libc::uint8_t, xlen: libc::size_t) -> libc::c_int {
-    let mut rng = thread_rng();
-    let buf = unsafe { std::slice::from_raw_parts_mut(buf, xlen as usize) };
-    for x in buf.iter_mut() {
-        *x = rng.gen();
-    }
+    let mut buf = unsafe { std::slice::from_raw_parts_mut(buf, xlen as usize) };
+    thread_rng().fill_bytes(&mut buf);
     0
 }
-
 
 #[cfg(test)]
 mod tests {
