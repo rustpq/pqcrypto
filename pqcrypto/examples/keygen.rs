e99bf48f69042adb556a3c4c15bd34edf3f436ef
Thom Wiggers <thom@thomwiggers.nl>
2019-05-22 11:43:58 +0200

    Add first example

---
+++ pqcrypto/examples/keygen.rs
@@ -0,0 +1,15 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+use pqcrypto::traits::sign::*;
+use pqcrypto::sign::sphincsharaka128frobust::*;
+
+
+fn main() -> std::io::Result<()> {
+    let mut pubfile = File::create("publickey.bin")?;
+    let mut secfile = File::create("secretkey.bin")?;
+    let (pk, sk) = keypair();
+    pubfile.write_all(pk.as_bytes())?;
+    secfile.write_all(sk.as_bytes())?;
+    Ok(())
+}
