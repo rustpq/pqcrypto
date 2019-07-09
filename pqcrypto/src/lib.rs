50ffcae47f121e64cde0cba6129f1bb160bef230
Thom Wiggers <thom@thomwiggers.nl>
2019-07-09 11:39:41 +0200

    don't import FFI dozens of times

--- pqcrypto/src/lib.rs
+++ pqcrypto/src/lib.rs
@@ -15,12 +15,28 @@ pub mod prelude {
 }
 
 pub mod kem {
-    pub use pqcrypto_frodo::*;
-    pub use pqcrypto_kyber::*;
-    pub use pqcrypto_ntru::*;
+    pub use pqcrypto_frodo::{
+        frodokem1344aes, frodokem1344shake, frodokem640aes, frodokem640shake, frodokem976aes,
+        frodokem976shake,
+    };
+    pub use pqcrypto_kyber::{kyber1024, kyber512, kyber768};
+    pub use pqcrypto_ntru::{ntruhps2048509, ntruhps2048677, ntruhps4096821, ntruhrss701};
 }
 
 pub mod sign {
-    pub use pqcrypto_mqdss::*;
-    pub use pqcrypto_sphincsplus::*;
+    pub use pqcrypto_mqdss::{mqdss48, mqdss64};
+    pub use pqcrypto_sphincsplus::{
+        sphincsharaka128frobust, sphincsharaka128fsimple, sphincsharaka128srobust,
+        sphincsharaka128ssimple, sphincsharaka192frobust, sphincsharaka192fsimple,
+        sphincsharaka192srobust, sphincsharaka192ssimple, sphincsharaka256frobust,
+        sphincsharaka256fsimple, sphincsharaka256srobust, sphincsharaka256ssimple,
+        sphincssha256128frobust, sphincssha256128fsimple, sphincssha256128srobust,
+        sphincssha256128ssimple, sphincssha256192frobust, sphincssha256192fsimple,
+        sphincssha256192srobust, sphincssha256192ssimple, sphincssha256256frobust,
+        sphincssha256256fsimple, sphincssha256256srobust, sphincssha256256ssimple,
+        sphincsshake256128frobust, sphincsshake256128fsimple, sphincsshake256128srobust,
+        sphincsshake256128ssimple, sphincsshake256192frobust, sphincsshake256192fsimple,
+        sphincsshake256192srobust, sphincsshake256192ssimple, sphincsshake256256frobust,
+        sphincsshake256256fsimple, sphincsshake256256srobust, sphincsshake256256ssimple,
+    };
 }
