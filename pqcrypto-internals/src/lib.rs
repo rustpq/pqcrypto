06b448f71e1fdd313967e7220e5d2b37a9d59366
Thom Wiggers <thom@thomwiggers.nl>
2021-07-28 17:17:04 +0200

    add some docs to randombytes function

--- pqcrypto-internals/src/lib.rs
+++ pqcrypto-internals/src/lib.rs
@@ -3,6 +3,10 @@ use core::slice;
 use getrandom;
 use libc;
 
+/// Get random bytes; exposed for PQClean implementations.
+///
+/// # Safety
+/// Assumes inputs are valid and may panic over FFI boundary if rng failed.
 #[no_mangle]
 pub unsafe extern "C" fn PQCRYPTO_RUST_randombytes(buf: *mut u8, len: libc::size_t) {
     let buf = slice::from_raw_parts_mut(buf, len as usize);
