42fb38fc5492035edc6d4501e85436e8567d43ce
Robin Winzler <robin.winzler@esrlabs.com>
2021-10-26 09:31:28 +0200

    Fix build.rs in pqcrypto-internals for cross-compilation

--- pqcrypto-internals/build.rs
+++ pqcrypto-internals/build.rs
@@ -4,8 +4,6 @@ use std::env;
 use std::path::Path;
 
 fn main() {
-    let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
-
     let includepath = Path::new("include").canonicalize().unwrap();
     println!("cargo:includepath={}", includepath.to_str().unwrap());
 
@@ -18,36 +16,44 @@ fn main() {
         cfiledir.join("sp800-185.c"),
     ];
 
-    if target_arch == "wasm32" {
-        let wasi_sdk_path = &std::env::var("WASI_SDK_DIR").expect("missing environment variable: WASI_SDK_DIR");
-        cc::Build::new()
-            .flag(format!("--sysroot={}", wasi_sdk_path).as_str())
-            .include(&includepath)
-            .files(common_files.into_iter())
-            .compile("pqclean_common");
-    } else {
-        cc::Build::new()
-            .include(&includepath)
-            .files(common_files.into_iter())
-            .compile("pqclean_common");
+    let mut build = cc::Build::new();
 
-        if cfg!(target_arch = "x86") || cfg!(target_arch = "x86_64") {
-            let mut builder = cc::Build::new();
-            if cfg!(target_env = "msvc") {
-                builder.flag("/arch:AVX2");
-            } else {
-                builder.flag("-mavx2");
-            };
-            builder
-                .file(
-                    &cfiledir
-                        .join("keccak4x")
-                        .join("KeccakP-1600-times4-SIMD256.c"),
-                )
-                .compile("keccak4x");
-            println!("cargo:rustc-link-lib=keccak4x");
+    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
+    if target_os == "wasi" {
+        let wasi_sdk_path =
+            &std::env::var("WASI_SDK_DIR").expect("missing environment variable: WASI_SDK_DIR");
+        build.flag(format!("--sysroot={}", wasi_sdk_path).as_str());
+    }
+
+    build
+        .include(&includepath)
+        .files(common_files.into_iter())
+        .compile("pqclean_common");
+    println!("cargo:rustc-link-lib=pqclean_common");
+
+    let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap_or_default();
+    if target_arch == "x86" || target_arch == "x86_64" {
+        let mut builder = cc::Build::new();
+
+        if target_os == "wasi" {
+            let wasi_sdk_path =
+                &std::env::var("WASI_SDK_DIR").expect("missing environment variable: WASI_SDK_DIR");
+            builder.flag(format!("--sysroot={}", wasi_sdk_path).as_str());
         }
 
-        println!("cargo:rustc-link-lib=pqclean_common");
+        let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
+        if target_os == "windows" {
+            builder.flag("/arch:AVX2");
+        } else {
+            builder.flag("-mavx2");
+        };
+        builder
+            .file(
+                &cfiledir
+                    .join("keccak4x")
+                    .join("KeccakP-1600-times4-SIMD256.c"),
+            )
+            .compile("keccak4x");
+        println!("cargo:rustc-link-lib=keccak4x")
     }
 }
