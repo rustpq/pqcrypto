50b6e6eb33acc899d2a2615d87d10e93816a33d0
Thom Wiggers <thom@thomwiggers.nl>
2025-02-04 17:10:12 +0100

    Bump version

--- pqcrypto-internals/src/lib.rs
+++ pqcrypto-internals/src/lib.rs
@@ -6,6 +6,15 @@ use core::slice;
 ///
 /// # Safety
 /// Assumes inputs are valid and may panic over FFI boundary if rng failed.
+///
+/// # Example
+/// ```rust
+/// use pqcrypto_internals::*;
+/// let mut buf = [0u8;10];
+/// unsafe {
+///   PQCRYPTO_RUST_randombytes(buf.as_mut_ptr(), buf.len());
+/// }
+/// ```
 #[no_mangle]
 pub unsafe extern "C" fn PQCRYPTO_RUST_randombytes(buf: *mut u8, len: libc::size_t) -> libc::c_int {
     let buf = slice::from_raw_parts_mut(buf, len);
